7d35dc40e1af4c6c12bff2f773cb37cc99dfe8b0
Robert Barham <barham.rl@gmail.com>
2014-03-29 21:15:23 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oneweekpst::Application.config.secret_key_base = '6ca891cbbf3fd4afd9a0cf73f0f4ba3db08c8ad5531c729c3e564eba0f5ab574701bbb4b9bae206e24a7a3d6d67698fdaaa283fd1ad8085a27500ab0486134d2'
